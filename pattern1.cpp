f97579ece654af9713fbe9f410fcabfb74f75f61
giripankaj <giripankaj21@gmail.com>
2024-02-06 17:51:06 +0530

    day2

---
+++ pattern1.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a=1;
+    int b;
+    int row=1;
+    cin>>b;
+    while(a<=b){
+        while(a<=row){
+            cout<<row<<" ";
+            a++;
+        }
+        row++;
+        cout<<endl;
+    }
+}
